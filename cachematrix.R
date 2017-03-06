c8847c46eda6822f151558b427b3b8958fe5c6c5
jltompkins91 <jltompkins91@gmail.com>
2017-03-06 10:17:36 -0500

    Programming Assignment 2 Submission

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,72 @@
-## Put comments here that give an overall description of what your
-## functions do
+##This function returns functions retrieving a matrix and it's inverse and for setting 
+## a matrix and its inverse.  The function returns a list of arguments corresponding to the functions
+## so they can be called by the $ index in the second function.
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
+makeCacheMatrix <- function(matCache = matrix()) {
+  #Create the setting functions:  
+  setMatrix <- function(aMatrix){     #pass invertible matrix, store in parent environment, clear past values
+    matCache <<- aMatrix
+    invCache <<- NULL
+  }
+  
+  setInverse <- function(inverse){    #pass matrix inverse as argument and store in the parent environment
+    invCache <<- inverse
+  }
+  
+  #Create the retrieving functions: 
+  getMatrix <- function(){      #return the matrix value
+    matCache
+  }
+  
+  getInverse <- function(){     #return the value of the inverse
+    invCache
+  }
+  
+  #Create the setting functions:  
+  setMatrix <- function(aMatrix){     #pass some invertible matrix and store in parent environment, clear past invCache values
+    matCache <<- aMatrix
+    invCache <<- NULL
+  }
+  
+  setInverse <- function(inverse){    #pass matrix inverse as argument and store in the parent environment
+    invCache <<- inverse
+  }
+  
+  #Create the retrieving functions: 
+  getMatrix <- function(){      #return the matrix value
+    matCache
+  }
+  
+  getInverse <- function(){     #return the value of the inverse
+    invCache
+  }
+  
+  
+  #Create a list in order to use the function after execution:
+  myFunctions <- list(getMatrix = getMatrix, getInverse = getInverse, setMatrix = setMatrix, setInverse = setInverse)
+  
 
 }
 
 
-## Write a short comment describing this function
+## This function first calls the getInverse function using $ to index and, if the cached inverse object is
+## not empty, it retrieves the value from makeCacheMatrix(). If empty it calculates the inverse using solve()
+## and returns the matrix inverse.
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+##  **NOTE**  Some results may be the scalar multiple of the inverse
+
+cacheSolve <- function(matCache, ...) {
+  #Call inverse retrieving function if it has a value stored:
+  invCache <- matCache$getInverse()
+  if (!is.null(invCache)){
+    message('Getting cached data')
+    return(invCache)
+  }
+  
+  #If the value of invCache is empty, calculate it and return the value:
+  data <- matCache$getMatrix()
+  invCache <- solve(data, ...)
+  matCache$setInverse(invCache)
+  invCache
+       
 }
